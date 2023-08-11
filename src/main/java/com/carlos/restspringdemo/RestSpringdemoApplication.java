0f64696171e780597ee70d06122a68fe0ad2ea18
ojcarlos <carlos.ojunior00@gmail.com>
2023-08-11 15:37:47 -0400

    project creaated

---
+++ src/main/java/com/carlos/restspringdemo/RestSpringdemoApplication.java
@@ -0,0 +1,13 @@
+package com.carlos.restspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlos/restspringdemo/RestSpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.restspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
